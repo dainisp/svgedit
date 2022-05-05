d8d25dff71e02622b46383d246039207709cd99e
Dainis Polis <dainisp@inbox.lv>
2022-05-05 13:52:23 +0300

    line adding

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <v-app>
     <v-main>
-    <div class="image-container"  @mousedown="mouseDown"  @mousemove="mouseMove" ><svg viewBox="0 0 500 500"  ref="svgroot" width="500px" height="500px"  xmlns="http://www.w3.org/2000/svg">
+<v-row>
+<v-col>
+<div class="image-container"  @mouseup="mouseDown"  @mousemove="mouseMove" ><svg viewBox="0 0 500 500"  ref="svgroot" width="500px" height="500px"  xmlns="http://www.w3.org/2000/svg">
   
 
   <!-- If you do not specify the stroke
        color the line will not be visible -->
 </svg>
-</div>  
+</div> 
+</v-col>
+<v-col>
+  <v-icon @click="insertLine" class="object-button" >mdi-edit</v-icon>
+<div>{{dataString}}</div>  
+</v-col>
+</v-row> 
     </v-main>
   </v-app>
 </template>
@@ -20,10 +28,19 @@ export default {
   data: () => ({
     //
     return :{
-      currentStatus:0
+      currentStatus:0,
+      dataString:'',
     }
   }),
 methods:{
+insertLine()
+{
+console.log("mouse insert")
+this.dataString = "koords x1:"  + this.$refs.svgroot.lastElementChild.attributes.x1
+                   +  " x2:" + this.$refs.svgroot.lastElementChild.attributes.x2
+                   " y1:" + this.$refs.svgroot.lastElementChild.attributes.y1
+                   " y2:" + this.$refs.svgroot.lastElementChild.attributes.y2
+},
 
 mouseMove(event)
 {
@@ -65,5 +82,12 @@ element.setAttribute('stroke','black')
   width:500px;
   height: 500px;
   border-width: 5px;
+  border-style:solid;
 }
+.object-button{
+ border-width: 5px;
+  border-style:solid;
+}
+
+
 </style>
